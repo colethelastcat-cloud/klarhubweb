9d12e01180ba8d2c0825affdf8d896ece0e2a2c7
colethelastcat-cloud <colethelastcat@gmail.com>
2025-08-31 23:33:10 -0400

    Update script.js

--- script.js
+++ script.js
@@ -83,7 +83,6 @@ const useAnimatedCounter = (target, duration = 2000) => {
     return [ref, count];
 };
 
-
 const useActiveNav = (headerHeight) => {
     const [activeSection, setActiveSection] = useState('home');
     useEffect(() => {
@@ -914,6 +913,18 @@ const PreviewModal = ({ onClose }) => {
         </Modal>
     );
 };
+
+const Logo = ({ onScrollTo }) => (
+    <svg 
+        onClick={() => onScrollTo('home')}
+        className="h-8 w-auto cursor-pointer" 
+        viewBox="0 0 100 100" 
+        fill="none" 
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path d="M12 10 L12 90 L28 90 L28 60 L60 90 L75 90 L40 50 L75 10 L60 10 L28 40 L28 10 L12 10 Z" className="fill-theme-primary stroke-theme-primary" strokeWidth="4"/>
+    </svg>
+);
 
 const Header = ({ headerRef, onScrollTo, onToggleMobileMenu, onTosClick, activeSection, isMobileMenuOpen, onGameClick, theme, setTheme }) => {
     const discordLink = "https://discord.gg/bGmGSnW3gQ";
